dd094f68ad3253f74b50c0e8572fa5decb5726e4
devusSs <flofest98@gmail.com>
2023-01-26 13:41:33 +0100

    reworked bot status page

--- client/src/pages/Status.tsx
+++ client/src/pages/Status.tsx
@@ -1,21 +1,33 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import "../App.css";
 import Button from "react-bootstrap/Button";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import axios from "../api/axios";
 
 const GetStatus = () => {
-  const [status, setStatus] = useState("Bot is currently not active.");
+  const [status, setStatus] = useState("No current status");
+  const [error, setError] = useState("");
   const [buttonText, setButtonText] = useState("Refetch Status");
   const [buttonActive, setButtonInactive] = useState(false);
 
   const getStatus = async () => {
+    setError("");
     try {
-      let url = import.meta.env.VITE_BASE_URL || "http://localhost:3000";
-      const resp = await fetch(url + "/status");
-      const respJson = await resp.json();
-      setStatus(respJson.data.status);
-    } catch (err) {}
+      const resp = await axios.get("/status");
+      if (resp.status !== 200) {
+        setStatus("API might be down, retry later");
+        setError("API might be down, retry later");
+      } else {
+        toast("Successfully refetched status.", { type: "success" });
+        setStatus(resp?.data?.data?.status);
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        setError("API might be down, retry later");
+        setStatus("Fetch error");
+      }
+    }
   };
 
   const revertButton = () => {
@@ -26,27 +38,23 @@ const GetStatus = () => {
   const refreshStatus = async () => {
     setStatus("Loading...");
     await getStatus();
-    toast("Successfully refetched status.", { type: "success" });
     setButtonText("Please wait 3 seconds before retrying");
     setButtonInactive(true);
     setTimeout(revertButton, 3000);
   };
 
-  useEffect((): any => {
+  useEffect(() => {
     getStatus();
   }, []);
 
+  useEffect(() => {
+    if (error !== "") {
+      toast(error, { type: "error" });
+    }
+  }, [error]);
+
   return (
     <div className="status">
-      <h1>{status}</h1>
-      <Button
-        className="fetch-button"
-        variant="primary"
-        onClick={refreshStatus}
-        disabled={buttonActive}
-      >
-        {buttonText}
-      </Button>
       <ToastContainer
         className="toast-container"
         position="top-right"
@@ -60,6 +68,15 @@ const GetStatus = () => {
         pauseOnHover
         theme="dark"
       />
+      <h1>{status}</h1>
+      <Button
+        className="fetch-button"
+        variant="primary"
+        onClick={refreshStatus}
+        disabled={buttonActive}
+      >
+        {buttonText}
+      </Button>
     </div>
   );
 };
