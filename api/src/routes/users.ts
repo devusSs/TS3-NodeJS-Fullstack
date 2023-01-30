167697593089cb7fdd7fe445ef9722cc6e8b23c7
devusSs <flofest98@gmail.com>
2023-01-30 09:58:00 +0100

    reworked query params for users

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -12,47 +12,83 @@ userRoutes.get("/", async (req: Request, res: Response) => {
   let latestUsername = req.query?.lu;
   let uniqueID = req.query?.uid;
 
-  if (latestUsername !== undefined || uniqueID !== undefined) {
-    let result: User[] | Error = await req.app
-      .get("db")
-      .getUsersByLUOrUID(latestUsername, uniqueID);
+  let result: User[] | Error = await req.app.get("db").getUsers();
 
-    if (result instanceof Error) {
-      let resp: ErrorResponse = {
-        code: 500,
-        error: { message: result.message },
-        timestamp: Temporal.Now.plainDateTimeISO(),
-      };
-      return res.status(500).json(resp);
-    }
-
-    if (result.length === 0) {
-      let resp: ErrorResponse = {
-        code: 404,
-        error: { message: "No corresponding user found." },
-        timestamp: Temporal.Now.plainDateTimeISO(),
-      };
-      return res.status(404).json(resp);
-    }
-
-    let resp: SuccessResponse = {
-      code: 200,
-      data: { result },
+  if (result instanceof Error) {
+    let resp: ErrorResponse = {
+      code: 500,
+      error: { message: result.message },
       timestamp: Temporal.Now.plainDateTimeISO(),
     };
-    return res.status(200).json(resp);
-  } else {
-    let result: User[] | Error = await req.app.get("db").getUsers();
+    return res.status(500).json(resp);
+  }
 
-    if (result instanceof Error) {
-      let resp: ErrorResponse = {
-        code: 500,
-        error: { message: result.message },
+  if (latestUsername !== undefined || uniqueID !== undefined) {
+    if (latestUsername !== undefined && uniqueID !== undefined) {
+      const filteredUsers = result.filter(
+        (user) =>
+          user.LatestUsername === latestUsername && user.UniqueID === uniqueID
+      );
+
+      if (filteredUsers.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding users found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredUsers,
         timestamp: Temporal.Now.plainDateTimeISO(),
       };
-      return res.status(500).json(resp);
+      return res.status(200).json(resp);
     }
 
+    if (latestUsername !== undefined) {
+      const filteredUsers = result.filter(
+        (user) => user.LatestUsername === latestUsername
+      );
+
+      if (filteredUsers.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding users found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredUsers,
+        timestamp: Temporal.Now.plainDateTimeISO(),
+      };
+      return res.status(200).json(resp);
+    }
+
+    if (uniqueID !== undefined) {
+      const filteredUsers = result.filter((user) => user.UniqueID === uniqueID);
+
+      if (filteredUsers.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding users found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredUsers,
+        timestamp: Temporal.Now.plainDateTimeISO(),
+      };
+      return res.status(200).json(resp);
+    }
+  } else {
     let resp: SuccessResponse = {
       code: 200,
       data: { result },
