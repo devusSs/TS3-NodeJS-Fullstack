1c3cb8eb18eab02ee0b1d7fc8523f2b3528f894a
devusSs <flofest98@gmail.com>
2023-01-30 09:41:58 +0100

    reworked query params for messages

--- api/src/routes/messages.ts
+++ api/src/routes/messages.ts
@@ -8,15 +8,12 @@ export const messagesRoutes = express.Router();
 
 messagesRoutes.use(requireAuth);
 
-// TODO: add filter by sent date
 messagesRoutes.get("/", async (req: Request, res: Response) => {
   let invokerUID = req?.query?.uid;
   let targetmode = req?.query?.tm;
 
   if (invokerUID !== undefined || targetmode !== undefined) {
-    let result: Message[] | Error = await req.app
-      .get("db")
-      .getMessagesByUIDOrTargetmode(invokerUID, targetmode);
+    let result: Message[] | Error = await req.app.get("db").getMessages();
 
     if (result instanceof Error) {
       let resp: ErrorResponse = {
@@ -24,24 +21,76 @@ messagesRoutes.get("/", async (req: Request, res: Response) => {
         error: { message: result.message },
         timestamp: Temporal.Now.plainDateTimeISO(),
       };
-      return res.status(500).json(resp);
+      res.status(500).json(resp);
+      return;
     }
 
-    if (result.length === 0) {
-      let resp: ErrorResponse = {
-        code: 404,
-        error: { message: "No corresponding message found." },
+    if (invokerUID !== undefined && targetmode !== undefined) {
+      const filteredMessages = result.filter(
+        (message) =>
+          message.InvokerUID === invokerUID && message.Targetmode == targetmode
+      );
+
+      if (filteredMessages.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding messages found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredMessages,
         timestamp: Temporal.Now.plainDateTimeISO(),
       };
-      return res.status(404).json(resp);
+      return res.status(200).json(resp);
     }
 
-    let resp: SuccessResponse = {
-      code: 200,
-      data: { result },
-      timestamp: Temporal.Now.plainDateTimeISO(),
-    };
-    return res.status(200).json(resp);
+    if (invokerUID !== undefined) {
+      const filteredMessages = result.filter(
+        (message) => message.InvokerUID === invokerUID
+      );
+
+      if (filteredMessages.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding messages found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredMessages,
+        timestamp: Temporal.Now.plainDateTimeISO(),
+      };
+      return res.status(200).json(resp);
+    }
+
+    if (targetmode !== undefined) {
+      const filteredMessages = result.filter(
+        (message) => message.Targetmode == targetmode
+      );
+
+      if (filteredMessages.length === 0) {
+        let resp: ErrorResponse = {
+          code: 404,
+          error: { message: "No corresponding messages found." },
+          timestamp: Temporal.Now.plainDateTimeISO(),
+        };
+        return res.status(404).json(resp);
+      }
+
+      let resp: SuccessResponse = {
+        code: 200,
+        data: filteredMessages,
+        timestamp: Temporal.Now.plainDateTimeISO(),
+      };
+      return res.status(200).json(resp);
+    }
   } else {
     let result: Message[] | Error = await req.app.get("db").getMessages();
 
